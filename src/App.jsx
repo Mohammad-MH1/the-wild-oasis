28f1bc0bf78b1171de99d3a2b082b82c28541db6
Mohammad Mohammadi <Mohammadyahoo15@gmail.com>
2024-08-22 15:51:48 +0330

    set up routes and pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,32 @@
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+
 import GlobalStyles from './styles/GlobalStyles';
+import Dashboard from './pages/Dashboard';
+import Bookings from './pages/Bookings';
+import Cabins from './pages/Cabins';
+import Users from './pages/Users';
+import Settings from './pages/Settings';
+import Account from './pages/Account';
+import Login from './pages/Login';
+import PageNotFound from './pages/PageNotFound';
 
 function App() {
   return (
     <>
       <GlobalStyles />
-      <h1>YO</h1>;
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Navigate to='dashboard' replace />} />
+          <Route path='dashboard' element={<Dashboard />} />
+          <Route path='bookings' element={<Bookings />} />
+          <Route path='cabins' element={<Cabins />} />
+          <Route path='users' element={<Users />} />
+          <Route path='settings' element={<Settings />} />
+          <Route path='account' element={<Account />} />
+          <Route path='login' element={<Login />} />
+          <Route path='*' element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
