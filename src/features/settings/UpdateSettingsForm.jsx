ea5dd90675fcb71abf1f7d19bb815da4af1a451c
Mohammad Mohammadi <Mohammadyahoo15@gmail.com>
2024-08-24 18:53:12 +0330

    create useSettings hook

--- src/features/settings/UpdateSettingsForm.jsx
+++ src/features/settings/UpdateSettingsForm.jsx
@@ -1,21 +1,43 @@
 import Form from '../../ui/Form';
 import FormRow from '../../ui/FormRow';
 import Input from '../../ui/Input';
+import { useSettings } from './useSettings';
+
+import Spinner from '../../ui/Spinner';
 
 function UpdateSettingsForm() {
+  const {
+    settings: {
+      minBookingLength,
+      maxBookingLength,
+      maxGuestsPerBooking,
+      breakfastPrice,
+    } = {},
+    isLoading,
+  } = useSettings();
+
+  if (isLoading) return <Spinner />;
   return (
     <Form>
       <FormRow label='Minimum nights/booking'>
-        <Input type='number' id='min-nights' />
+        <Input type='number' id='min-nights' defaultValue={minBookingLength} />
       </FormRow>
       <FormRow label='Maximum nights/booking'>
-        <Input type='number' id='max-nights' />
+        <Input type='number' id='max-nights' defaultValue={maxBookingLength} />
       </FormRow>
       <FormRow label='Maximum guests/booking'>
-        <Input type='number' id='max-guests' />
+        <Input
+          type='number'
+          id='max-guests'
+          defaultValue={maxGuestsPerBooking}
+        />
       </FormRow>
       <FormRow label='Breakfast price'>
-        <Input type='number' id='breakfast-price' />
+        <Input
+          type='number'
+          id='breakfast-price'
+          defaultValue={breakfastPrice}
+        />
       </FormRow>
     </Form>
   );
